d40a7be52234bb8b79e68c8afe61fc58ba75225e
sseung <sseutta@naver.com>
2025-11-18 14:16:34 +0900

    feat: 라우터 설정

--- lib/routing/routing_config.dart
+++ lib/routing/routing_config.dart
@@ -9,14 +9,25 @@ class RoutingConfig {
       ShellRoute(
         builder: (context, state, child) => NavigationScreen(),
         routes: [
-          GoRoute(path: '/home', builder: (_, __) => HomeScreen(),),
-          GoRoute(path: '/exhibition', builder: (_, __) => PlaceholderScreen(title: "전시"),),
-          GoRoute(path: '/collection', builder: (_, __) => PlaceholderScreen(title: "조각"),),
-          GoRoute(path: '/program', builder: (_, __) => PlaceholderScreen(title: "교육프로그램"),),
+          GoRoute(path: '/home', builder: (_, __) => HomeScreen()),
+          GoRoute(
+            path: '/exhibition',
+            builder: (_, __) => PlaceholderScreen(title: "전시"),
+          ),
+          GoRoute(
+            path: '/collection',
+            builder: (_, __) => PlaceholderScreen(title: "조각"),
+          ),
+          GoRoute(
+            path: '/program',
+            builder: (_, __) => PlaceholderScreen(title: "교육프로그램"),
+          ),
         ],
       ),
-      GoRoute(path: '/login', builder: (_, __) => PlaceholderScreen(title: "로그인"),)
+      GoRoute(
+        path: '/login',
+        builder: (_, __) => PlaceholderScreen(title: "로그인"),
+      ),
     ],
-
   );
 }
