de280351cbae9b5439b225c8a06fdbb9bb89205e
sseung <sseutta@naver.com>
2025-11-19 13:50:40 +0900

    feat: 교육 api
    getPrograms

--- lib/data/data_sources/soma_api.dart
+++ lib/data/data_sources/soma_api.dart
@@ -2,11 +2,13 @@ import 'dart:developer';
 
 import 'package:soma_museum_app/core/env_config.dart';
 import 'package:soma_museum_app/core/network/dio_client.dart';
+import 'package:soma_museum_app/data/model/program/program.dart';
 import 'package:soma_museum_app/data/model/sculpture/sculpture.dart';
 
 class SomaApi {
   final _dio = DioClient();
 
+  // 조각 정보 조회
   Future<List<Sculpture>> getSculptures({
     int pageNo = 1,
     int numOfRows = 20,
@@ -25,9 +27,39 @@ class SomaApi {
       log('getSculptures API Response : $response');
 
       final result =
-          (response.data['response']['body']['items']['item'] as List)
-              .map((e) => Sculpture.fromJson(e))
-              .toList();
+      (response.data['response']['body']['items']['item'] as List)
+          .map((e) => Sculpture.fromJson(e))
+          .toList();
+
+      return result;
+    } catch (e, s) {
+      log('$e, $s');
+      rethrow;
+    }
+  }
+
+  // 교육 프로그램 조회
+  Future<List<Program>> getPrograms({
+    int pageNo = 1,
+    int numOfRows = 20,
+  }) async {
+    try {
+      final response = await _dio.get(
+        'https://apis.data.go.kr/B551014/SRVC_OD_API_EDCN_CLASS/todz_api_edcn_class_i',
+        query: {
+          'serviceKey': EnvConfig.apiKey,
+          'pageNo': pageNo,
+          'numOfRows': numOfRows,
+          'resultType': 'json',
+        },
+      );
+
+      log('getPrograms API Response : $response');
+
+      final result =
+      (response.data['response']['body']['items']['item'] as List)
+          .map((e) => Program.fromJson(e))
+          .toList();
 
       return result;
     } catch (e, s) {
