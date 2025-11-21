af8a7bed0083026924aa91f1a3fb95005dec6d44
chojja7188 <shoun7188@naver.com>
2025-11-21 11:39:11 +0900

    feat: DisplayProvider 수정. 로컬DB에 저장했던걸 꺼내거나, 없으면 API 호출

--- lib/presentation/providers/display/display_provider.dart
+++ lib/presentation/providers/display/display_provider.dart
@@ -1,7 +1,12 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:soma_museum_app/data/data_sources/objectbox_data_source.dart';
 import 'package:soma_museum_app/data/data_sources/soma_api.dart';
+import 'package:soma_museum_app/data/model/display/dto/display.dart';
+import 'package:soma_museum_app/data/model/display/entity/display_entity.dart';
+import 'package:soma_museum_app/data/model/display/mapper/display_mapper.dart';
 import 'package:soma_museum_app/presentation/providers/display/display_state.dart';
 
 final displayProvider = AsyncNotifierProvider<DisplayNotifier, DisplayState>(
@@ -10,11 +15,45 @@ final displayProvider = AsyncNotifierProvider<DisplayNotifier, DisplayState>(
 
 class DisplayNotifier extends AsyncNotifier<DisplayState> {
   final _somaApi = SomaApi();
+  final _objectboxDataSource = ObjectboxDataSource.instance;
 
   @override
   Future<DisplayState> build() async {
-    final displays = await _somaApi.getDisplays();
+    try {
+      final displays = await _getDisplays();
 
-    return DisplayState(displays: displays);
+      return DisplayState(displays: displays);
+    } catch (e) {
+      return DisplayState(displays: []);
+    }
+  }
+
+  Future<List<Display>> _getDisplays() async {
+    try {
+      final displayEntities = _objectboxDataSource.getAll<DisplayEntity>();
+
+      // 로컬에 저장된 값이 있으면 그대로 리턴
+      if (displayEntities.isNotEmpty) {
+        final isExpired =
+            DateTime.now().difference(displayEntities.first.updatedAt).inHours >
+            24;
+
+        if (!isExpired) {
+          return displayEntities.map((e) => e.toDto()).toList();
+        }
+      }
+      _objectboxDataSource.clear<DisplayEntity>();
+      final displays = await _somaApi.getDisplays();
+      _objectboxDataSource.saveList<DisplayEntity>(
+        displays.map((e) => e.toEntity()).toList(),
+      );
+
+      final newDisplayEntities = _objectboxDataSource.getAll<DisplayEntity>();
+
+      return newDisplayEntities.map((e) => e.toDto()).toList();
+    } catch (e) {
+      log('getDisplays Error : $e');
+      rethrow;
+    }
   }
 }
