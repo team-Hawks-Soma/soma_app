9cd78beb3e1d8635a950dd08138f079dcaa4b4c3
chojja7188 <shoun7188@naver.com>
2025-11-21 09:57:51 +0900

    feat: ObjectboxDataSource 기본 작업 및 main에서 init

---
+++ lib/data/data_sources/objectbox_data_source.dart
@@ -0,0 +1,28 @@
+import 'package:soma_museum_app/objectbox.g.dart';
+
+class ObjectboxDataSource {
+  late final Store _store;
+
+  // 싱글톤 패턴
+  ObjectboxDataSource._internal();
+
+  static final ObjectboxDataSource instance = ObjectboxDataSource._internal();
+
+  Future<void> init() async {
+    _store = await openStore();
+  }
+
+  Box<T> box<T>() => _store.box<T>();
+
+  int save<T>(T entity) {
+    return box<T>().put(entity);
+  }
+
+  List<int> saveList<T>(List<T> entities) {
+    return box<T>().putMany(entities);
+  }
+
+  List<T> getAll<T>() {
+    return box<T>().getAll();
+  }
+}
